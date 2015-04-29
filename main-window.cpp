9ada790508ea99a875c248ea32130cc8adf223c4
Loïc Bourgois <loicbourgois.web@gmail.com>
2015-04-29 21:53:21 +0200

    Refactor

--- main-window.cpp
+++ main-window.cpp
@@ -4,34 +4,40 @@
 #include <QDate>
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow),
-    databaseManager(Util::getLineFromConf("databaseName"))
+  QMainWindow(parent),
+  ui(new Ui::MainWindow),
+  databaseManager(Util::getLineFromConf("databaseName"))
 {
-    ui->setupUi(this);
-    if(!databaseManager.connect())
-    {
-        if(databaseManager.createDatabase())
-            databaseManager.connect();
-    }
-    bool doErase = false;
-    if(Util::getLineFromConf("erase") == "true")
-        doErase = true;
-    if(Util::getLineFromConf("download") == "true" && Util::getLineFromConf("add") == "true")
-        databaseManager.downloadAndAdd(QDate::fromString(Util::getLineFromConf("startDate"),"yyyy-MM-dd")
-                                       , QDate::fromString(Util::getLineFromConf("endDate"),"yyyy-MM-dd")
-                                       , doErase);
-    else if(Util::getLineFromConf("download") == "false" && Util::getLineFromConf("add") == "true")
-        databaseManager.add(QDate::fromString(Util::getLineFromConf("startDate"),"yyyy-MM-dd")
-                            , QDate::fromString(Util::getLineFromConf("endDate"),"yyyy-MM-dd")
-                            , doErase);
-    else if(Util::getLineFromConf("download") == "true" && Util::getLineFromConf("add") == "false")
-        databaseManager.download(QDate::fromString(Util::getLineFromConf("startDate"),"yyyy-MM-dd")
-                                 , QDate::fromString(Util::getLineFromConf("endDate"),"yyyy-MM-dd")
-                                 , doErase);
+  ui->setupUi(this);
+  if(!databaseManager.connect())
+  {
+    if(databaseManager.createDatabase())
+      databaseManager.connect();
+  }
+  bool doErase = false;
+  if(Util::getLineFromConf("erase") == "true")
+    doErase = true;
+  if(Util::getLineFromConf("download") == "true"
+     && Util::getLineFromConf("add") == "true")
+    databaseManager.downloadAndAdd(
+          QDate::fromString(Util::getLineFromConf("startDate"),"yyyy-MM-dd"),
+          QDate::fromString(Util::getLineFromConf("endDate"),"yyyy-MM-dd"),
+          doErase);
+  else if(Util::getLineFromConf("download") == "false"
+          && Util::getLineFromConf("add") == "true")
+    databaseManager.add(
+          QDate::fromString(Util::getLineFromConf("startDate"),"yyyy-MM-dd"),
+          QDate::fromString(Util::getLineFromConf("endDate"),"yyyy-MM-dd"),
+          doErase);
+  else if(Util::getLineFromConf("download") == "true"
+          && Util::getLineFromConf("add") == "false")
+    databaseManager.download(
+          QDate::fromString(Util::getLineFromConf("startDate"),"yyyy-MM-dd"),
+          QDate::fromString(Util::getLineFromConf("endDate"),"yyyy-MM-dd"),
+          doErase);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
 }
