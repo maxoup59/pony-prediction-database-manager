61faf4c62ed6297ddd8e09606ebc69029f2bc845
Maxime <azamanama@gmail.com>
2015-04-29 21:31:40 +0200

    Add date,doerase,download,add to config.xml

--- main-window.cpp
+++ main-window.cpp
@@ -1,22 +1,37 @@
 #include "main-window.hpp"
 #include "ui_main-window.h"
+
 #include <QDate>
 
 MainWindow::MainWindow(QWidget *parent) :
-  QMainWindow(parent),
-  ui(new Ui::MainWindow),
-  databaseManager("ponyprediction")
+    QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    databaseManager("ponyprediction")
 {
-  ui->setupUi(this);
-  if(!databaseManager.connect())
-  {
-    if(databaseManager.createDatabase())
-      databaseManager.connect();
-  }
-  databaseManager.add(QDate(2015,01,01), QDate(2015,12,31), true);
+    ui->setupUi(this);
+    if(!databaseManager.connect())
+    {
+        if(databaseManager.createDatabase())
+            databaseManager.connect();
+    }
+    bool doErase = false;
+    if(Util::getLineFromConf("erase") == "true")
+        doErase = true;
+    if(Util::getLineFromConf("download") == "true" && Util::getLineFromConf("add") == "true")
+        databaseManager.downloadAndAdd(QDate::fromString(Util::getLineFromConf("startDate"),"yyyy-MM-dd")
+                                       , QDate::fromString(Util::getLineFromConf("endDate"),"yyyy-MM-dd")
+                                       , doErase);
+    else if(Util::getLineFromConf("download") == "false" && Util::getLineFromConf("add") == "true")
+        databaseManager.add(QDate::fromString(Util::getLineFromConf("startDate"),"yyyy-MM-dd")
+                            , QDate::fromString(Util::getLineFromConf("endDate"),"yyyy-MM-dd")
+                            , doErase);
+    else if(Util::getLineFromConf("download") == "true" && Util::getLineFromConf("add") == "false")
+        databaseManager.download(QDate::fromString(Util::getLineFromConf("startDate"),"yyyy-MM-dd")
+                                 , QDate::fromString(Util::getLineFromConf("endDate"),"yyyy-MM-dd")
+                                 , doErase);
 }
 
 MainWindow::~MainWindow()
 {
-  delete ui;
+    delete ui;
 }
